02829c5ffdf1d1231709a9e6f7a783fa868cea30
tgordienko <tgordienko@larch-networks.com>
2019-09-23 19:48:39 +0300

    Phonebook backend step1

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  }
+];
+
+// const generateId = () => {
+//   const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
+//   return maxId + 1;
+// };
+
+// app.use(bodyParser.json());
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello</h1>");
+// });
+
+app.get("/persons", (req, res) => {
+  res.json(persons);
+});
+
+// app.get("/notes/:id", (request, response) => {
+//   const id = Number(request.params.id);
+//   const note = notes.find(note => note.id === id);
+
+//   if (note) {
+//     response.json(note);
+//   } else {
+//     response.status(404).end();
+//   }
+// });
+
+// app.delete("/notes/:id", (request, response) => {
+//   const id = Number(request.params.id);
+//   notes = notes.filter(note => note.id !== id);
+
+//   response.status(204).end();
+// });
+
+// app.post("/notes", (request, response) => {
+//   const body = request.body;
+
+//   if (!body.content) {
+//     return response.status(400).json({
+//       error: "content missing"
+//     });
+//   }
+
+//   const note = {
+//     content: body.content,
+//     important: body.important || false,
+//     date: new Date(),
+//     id: generateId()
+//   };
+
+//   notes = notes.concat(note);
+
+//   response.json(note);
+// });
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

0b26691bc3e1cdfa082f805cab26c01dfc9405e8
tgordienko <tgordienko@larch-networks.com>
2019-09-23 20:16:56 +0300

    Phonebook backend step5

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ let persons = [
   }
 ];
 
-// const generateId = () => {
-//   const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
-//   return maxId + 1;
-// };
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map(n => n.id)) : 0;
+  return maxId + 1;
+};
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 app.get("/api/persons", (req, res) => {
   res.json(persons);
@@ -50,7 +50,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-app.get("/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   const person = persons.find(person => person.id === id);
 
@@ -61,26 +61,26 @@ app.get("/persons/:id", (request, response) => {
   }
 });
 
-// app.post("/notes", (request, response) => {
-//   const body = request.body;
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  console.log(request.body);
 
-//   if (!body.content) {
-//     return response.status(400).json({
-//       error: "content missing"
-//     });
-//   }
+  if (!body.name) {
+    return response.status(400).json({
+      error: "content missing"
+    });
+  }
 
-//   const note = {
-//     content: body.content,
-//     important: body.important || false,
-//     date: new Date(),
-//     id: generateId()
-//   };
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  };
 
-//   notes = notes.concat(note);
+  persons = persons.concat(person);
 
-//   response.json(note);
-// });
+  response.json(person);
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
